5fb003db5130bf7a30cd5494142db65533b5c124
teruteru <tutituti3080@gmail.com>
2023-06-14 08:48:27 +0900

    Add: Vitestの設定を記述

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  server: {
+    open: true,
+  },
   plugins: [react(), tsconfigPaths()],
+  test: {
+    // テスト環境を指定
+    environment: "happy-dom",
+    // API をグローバルに使う
+    globals: true,
+  },
 });
