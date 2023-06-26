da004160ff28225f12719e19c5d9104c6b1bf9ea
teruteru <tutituti3080@gmail.com>
2023-06-26 09:31:08 +0900

    Add: 共通コンポーネントの検索欄を追加

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,23 @@
+import { TextInput } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
+import { Dispatch, SetStateAction } from "react";
+
+type SearchInputProps = {
+  filterInput: string;
+  setFilterInput: Dispatch<SetStateAction<string>>;
+};
+
+const SearchInput = ({ filterInput, setFilterInput }: SearchInputProps) => {
+  return (
+    <TextInput
+      icon={<IconSearch size={18} stroke={1.5} />}
+      radius="lg"
+      size="sm"
+      placeholder="キーワードを入力..."
+      value={filterInput}
+      onChange={(e) => setFilterInput(e.target.value)}
+    />
+  );
+};
+
+export default SearchInput;
