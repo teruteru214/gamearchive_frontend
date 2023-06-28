05d33a314721cc775dc1fb8f3e4c58b8e1b1d85d
teruteru <tutituti3080@gmail.com>
2023-06-28 13:44:31 +0900

    Fix:　不要なコードを削除

--- src/components/features/top/Top.tsx
+++ src/components/features/top/Top.tsx
@@ -1,24 +1,6 @@
 import AppDescription from "./AppDescription";
 import HeroContents from "./HeroContents";
 
-const data = [
-  {
-    link: "#",
-    label: "プライバシーポリシー",
-  },
-  {
-    link: "#",
-    label: "利用規約",
-  },
-  {
-    link: "#",
-    label: "お問い合わせ",
-  },
-];
-
-const isLogin = true;
-const user = { name: "User", image: "user-image-url" };
-
 const Top = () => {
   return (
     <>
