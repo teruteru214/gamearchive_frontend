935e4de7ee7beac95b94107239993a0eee01b070
teruteru <tutituti3080@gmail.com>
2023-07-04 08:46:30 +0900

    Chore: Vite-plugin-svgrを削除

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     // テスト環境を指定
     environment: "happy-dom",
