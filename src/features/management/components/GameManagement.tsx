37e763ed8af899fec8ee12f5f87edf386b317758
teruteru <tutituti3080@gmail.com>
2023-07-09 19:52:52 +0900

    feat: managementページのボタンにリンク先を追加

--- src/features/management/components/GameManagement.tsx
+++ src/features/management/components/GameManagement.tsx
@@ -1,18 +1,21 @@
 import { Button, Container } from "@mantine/core";
 import { IconDisc } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
 
 import Profile from "../../../components/Profile";
 import GameManagementField from "./GameManagementField";
 import HeroContents from "./HeroContents";
 
 const GameManagement = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <HeroContents />
       <Container>
         <Profile />
         <div className="flex justify-center">
-          <Button>
+          <Button onClick={() => navigate("/acquisition")}>
             <IconDisc size="0.9rem" stroke={1.5} className="mr-1" />
             ゲームを追加する
           </Button>
