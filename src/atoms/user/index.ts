cca31ba01bd58d75f3258ba2decf8df86e2fb1c4
teruteru <tutituti3080@gmail.com>
2023-07-10 11:25:49 +0900

    Remove: 不要な型定義を削除

--- src/atoms/user/index.ts
+++ src/atoms/user/index.ts
@@ -1,13 +1,5 @@
 import { atom } from "jotai";
-
-export type User = {
-  username: string;
-  avatar: string;
-  uid: string;
-  introduction: string;
-  twitter_username: string;
-  visibility: number;
-};
+import { User } from "types/user";
 
 export const loginUserAtom = atom<User | null>(null);
 
