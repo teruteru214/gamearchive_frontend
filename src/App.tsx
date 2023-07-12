2e78a417d797dbdbf54c95adaf17bfd462fb3870
teruteru <tutituti3080@gmail.com>
2023-07-12 11:33:58 +0900

    feat: ServiceWorkerが本番環境で動作しないように

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,24 @@ import AppRoutes from "routes";
 
 const App = () => {
   useEffect(() => {
-    axios.get(endpoint).then((res) => console.log(res));
+    const fetchUnplayedGames = async () => {
+      const res = await axios.get(`${endpoint}/management/status/unplayed`);
+      console.log(res.data);
+    };
+
+    const fetchPlayingGames = async () => {
+      const res = await axios.get(`${endpoint}/management/status/playing`);
+      console.log(res.data);
+    };
+
+    const fetchClearedGames = async () => {
+      const res = await axios.get(`${endpoint}/management/status/clear`);
+      console.log(res.data);
+    };
+
+    fetchUnplayedGames();
+    fetchPlayingGames();
+    fetchClearedGames();
   }, []);
 
   return (
