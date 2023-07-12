ad7ea240501f850e00853e297f5090672eef55fb
teruteru <tutituti3080@gmail.com>
2023-07-12 15:51:48 +0900

    Fix: データを一意に保つため修正

--- src/test/handlers/acquisitionGames.ts
+++ src/test/handlers/acquisitionGames.ts
@@ -41,24 +41,24 @@ const initialData = [
     id: 2,
     name: "pokemon Moon",
     cover: {
-      id: 1,
+      id: 2,
       url: "https://images.igdb.com/igdb/image/upload/t_cover_big/co3co8.png",
     },
     genres: [
       {
-        id: 1,
+        id: 5,
         name: "Adventure",
       },
       {
-        id: 2,
+        id: 6,
         name: "RPG",
       },
       {
-        id: 3,
+        id: 7,
         name: "Strategy",
       },
       {
-        id: 3,
+        id: 8,
         name: "TBS",
       },
     ],
@@ -80,15 +80,15 @@ const initialData = [
     },
     genres: [
       {
-        id: 1,
+        id: 9,
         name: "Adventure",
       },
       {
-        id: 2,
+        id: 10,
         name: "RPG",
       },
       {
-        id: 4,
+        id: 11,
         name: "TBS",
       },
     ],
@@ -114,23 +114,23 @@ const initialData = [
     },
     genres: [
       {
-        id: 1,
+        id: 12,
         name: "Adventure",
       },
       {
-        id: 2,
+        id: 13,
         name: "RPG",
       },
       {
-        id: 3,
+        id: 14,
         name: "Strategy",
       },
       {
-        id: 4,
+        id: 15,
         name: "TBS",
       },
       {
-        id: 5,
+        id: 16,
         name: "Indie",
       },
     ],
@@ -162,268 +162,6 @@ const initialData = [
     ],
     url: "https://www.igdb.com/games/undertale",
     rating: 87,
-  },
-  {
-    id: 5,
-    name: "pokemon Sun",
-    cover: {
-      id: 1,
-      url: "https://images.igdb.com/igdb/image/upload/t_cover_big/co3co7.png",
-    },
-    genres: [
-      {
-        id: 1,
-        name: "Adventure",
-      },
-      {
-        id: 2,
-        name: "RPG",
-      },
-      {
-        id: 3,
-        name: "Strategy",
-      },
-      {
-        id: 4,
-        name: "TBS",
-      },
-    ],
-    platforms: [
-      {
-        id: 1,
-        name: "Nintendo3DS",
-      },
-    ],
-    url: "https://www.igdb.com/games/pokemon-sun",
-    rating: 76,
-  },
-  {
-    id: 6,
-    name: "pokemon Moon",
-    cover: {
-      id: 1,
-      url: "https://images.igdb.com/igdb/image/upload/t_cover_big/co3co8.png",
-    },
-    genres: [
-      {
-        id: 1,
-        name: "Adventure",
-      },
-      {
-        id: 2,
-        name: "RPG",
-      },
-      {
-        id: 3,
-        name: "Strategy",
-      },
-      {
-        id: 3,
-        name: "TBS",
-      },
-    ],
-    platforms: [
-      {
-        id: 1,
-        name: "Nintendo3DS",
-      },
-    ],
-    url: "https://www.igdb.com/games/pokemon-moon",
-    rating: 78,
-  },
-  {
-    id: 7,
-    name: "pokemon Gold",
-    cover: {
-      id: 3,
-      url: "https://images.igdb.com/igdb/image/upload/t_cover_big/co5pia.png",
-    },
-    genres: [
-      {
-        id: 1,
-        name: "Adventure",
-      },
-      {
-        id: 2,
-        name: "RPG",
-      },
-      {
-        id: 4,
-        name: "TBS",
-      },
-    ],
-    platforms: [
-      {
-        id: 1,
-        name: "Nintendo3DS",
-      },
-      {
-        id: 2,
-        name: "Gameboycolor",
-      },
-    ],
-    url: "https://www.igdb.com/games/pokemon-gold",
-    rating: 86,
-  },
-  {
-    id: 8,
-    name: "Undertale",
-    cover: {
-      id: 4,
-      url: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2855.png",
-    },
-    genres: [
-      {
-        id: 1,
-        name: "Adventure",
-      },
-      {
-        id: 2,
-        name: "RPG",
-      },
-      {
-        id: 3,
-        name: "Strategy",
-      },
-      {
-        id: 4,
-        name: "TBS",
-      },
-      {
-        id: 5,
-        name: "Indie",
-      },
-    ],
-    platforms: [
-      {
-        id: 3,
-        name: "Xbox One",
-      },
-      {
-        id: 4,
-        name: "PS4",
-      },
-      {
-        id: 5,
-        name: "Linux",
-      },
-      {
-        id: 6,
-        name: "Mac",
-      },
-      {
-        id: 7,
-        name: "PSVita",
-      },
-      {
-        id: 8,
-        name: "PC(windows)",
-      },
-    ],
-    url: "https://www.igdb.com/games/undertale",
-    rating: 87,
-  },
-  {
-    id: 9,
-    name: "pokemon Sun",
-    cover: {
-      id: 1,
-      url: "https://images.igdb.com/igdb/image/upload/t_cover_big/co3co7.png",
-    },
-    genres: [
-      {
-        id: 1,
-        name: "Adventure",
-      },
-      {
-        id: 2,
-        name: "RPG",
-      },
-      {
-        id: 3,
-        name: "Strategy",
-      },
-      {
-        id: 4,
-        name: "TBS",
-      },
-    ],
-    platforms: [
-      {
-        id: 1,
-        name: "Nintendo3DS",
-      },
-    ],
-    url: "https://www.igdb.com/games/pokemon-sun",
-    rating: 76,
-  },
-  {
-    id: 10,
-    name: "pokemon Moon",
-    cover: {
-      id: 1,
-      url: "https://images.igdb.com/igdb/image/upload/t_cover_big/co3co8.png",
-    },
-    genres: [
-      {
-        id: 1,
-        name: "Adventure",
-      },
-      {
-        id: 2,
-        name: "RPG",
-      },
-      {
-        id: 3,
-        name: "Strategy",
-      },
-      {
-        id: 3,
-        name: "TBS",
-      },
-    ],
-    platforms: [
-      {
-        id: 1,
-        name: "Nintendo3DS",
-      },
-    ],
-    url: "https://www.igdb.com/games/pokemon-moon",
-    rating: 78,
-  },
-  {
-    id: 11,
-    name: "pokemon Moon",
-    cover: {
-      id: 1,
-      url: "https://images.igdb.com/igdb/image/upload/t_cover_big/co3co8.png",
-    },
-    genres: [
-      {
-        id: 1,
-        name: "Adventure",
-      },
-      {
-        id: 2,
-        name: "RPG",
-      },
-      {
-        id: 3,
-        name: "Strategy",
-      },
-      {
-        id: 3,
-        name: "TBS",
-      },
-    ],
-    platforms: [
-      {
-        id: 1,
-        name: "Nintendo3DS",
-      },
-    ],
-    url: "https://www.igdb.com/games/pokemon-moon",
-    rating: 78,
   },
 ];
 
