f1980aff1121082fbbd404833e6fa9e93c1800e9
teruteru <tutituti3080@gmail.com>
2023-07-13 10:04:21 +0900

    Fix: ステータスの値が次のゲーム取得時に残っているバグを修正

--- src/features/status/components/StatusModal.tsx
+++ src/features/status/components/StatusModal.tsx
@@ -9,6 +9,7 @@ import { useAtom } from "jotai";
 import { FC, useEffect } from "react";
 import { z } from "zod";
 
+// カスタムフックから取得するPropsを定義
 type StatusModalProps = {
   opened: boolean;
   onClose: () => void;
@@ -16,14 +17,17 @@ type StatusModalProps = {
 };
 
 const StatusModal: FC<StatusModalProps> = ({ opened, onClose, game }) => {
+  // jotaiで作成したstateを取得
   const [status, setStatus] = useAtom(statusAtom);
   const [convertedGame, setConvertedGame] = useAtom(convertedGameAtom);
   const [error, setError] = useAtom(errorAtom);
 
+  // zodを用いてステータスのバリデーションを定義
   const StatusSchema = z
     .string()
     .nonempty({ message: "ゲームステータスを選択してください" });
 
+  // ゲーム情報と選択したステータスを元に変換したゲームデータを作成
   useEffect(() => {
     if (status) {
       setConvertedGame({
@@ -45,34 +49,54 @@ const StatusModal: FC<StatusModalProps> = ({ opened, onClose, game }) => {
     }
   }, [status, game]);
 
+  // ゲームを取得するボタンのクリックハンドラー
   const handleClick = () => {
-    if (convertedGame && status) {
+    // ステータスが存在する場合のみバリデーションと変換処理を実行
+    if (status) {
       // バリデーション
       const result = StatusSchema.safeParse(status);
       if (result.success) {
         console.log(convertedGame);
         setError(null); // エラーをリセット
       } else {
-        setError(result.error.message);
+        setError(result.error.message); // バリデーションエラーメッセージをセット
       }
     } else {
-      setError("※ゲームステータスを選択してください");
+      setError("※ゲームステータスを選択してください"); // ステータスがない場合のエラーメッセージをセット
     }
   };
 
+  // モーダルを閉じる際にエラーとステータスをリセット
+  const handleClose = () => {
+    setError(null);
+    setStatus(null);
+    onClose();
+  };
+
+  // モーダルが開かれる度にエラーとステータスをリセット
+  useEffect(() => {
+    if (opened) {
+      setError(null);
+      setStatus(null);
+    }
+  }, [opened]);
+
+  // ステータスが選択された際にエラーメッセージをリセット
+  const handleStatusChange = (value: string) => {
+    setStatus(value);
+    setError(null);
+  };
+
+  // モーダルのレンダリング
   return (
-    <Modal opened={opened} onClose={onClose} centered size="sm">
+    <Modal opened={opened} onClose={handleClose} centered size="sm">
       <Stack className="flex items-center justify-center space-y-4 pb-16">
         <Text size="lg">ゲームステータスを入力</Text>
-        {error && (
-          <Text color="red" size="xs">
-            {error}
-          </Text>
-        )}
         <Select
           placeholder="ゲームステータスを選択"
           data={["積みゲー", "プレイ中", "クリア"]}
-          onChange={(value) => setStatus(value)}
+          onChange={handleStatusChange} // ステータスが選択された際のハンドラー
+          error={error ? "※ゲームステータスを選択してください" : null} // エラーメッセージ
         />
         <Button onClick={handleClick}>ゲームを取得する</Button>
       </Stack>
