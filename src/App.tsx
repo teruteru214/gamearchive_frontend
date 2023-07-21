c81a67936f6a290868e240b9f8ded9d4e326edb9
teruteru <tutituti3080@gmail.com>
2023-07-21 09:25:08 +0900

    Feat: RailsAPIとの連携を確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
+import { endpoint } from "config";
 import AppProvider from "providers/app";
+import { useEffect } from "react";
 import AppRoutes from "routes";
 
 const App = () => {
+  useEffect(() => {
+    axios
+      .get(`${endpoint}/api/v1/hello`)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <AppProvider>
       <AppRoutes />
