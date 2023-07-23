4a50639b4f2f288d3de04677ca13d2d494fed196
teruteru <tutituti3080@gmail.com>
2023-07-23 13:57:06 +0900

    test: RailsAPIとの連携を確認するコードを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import axios from "axios";
-import { endpoint } from "config";
 import AppProvider from "providers/app";
-import { useEffect } from "react";
 import AppRoutes from "routes";
 
 const App = () => {
-  useEffect(() => {
-    axios
-      .get(`${endpoint}/api/v1/hello`)
-      .then((res) => console.log(res.data))
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
     <AppProvider>
       <AppRoutes />
