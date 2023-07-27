a8b930feb1827e082e50e4e5ffba82eff7c74255
teruteru <tutituti3080@gmail.com>
2023-07-27 15:23:59 +0900

    refactor: ログインボタンでローディングを表示できるようにする

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -10,13 +10,19 @@ import { NavigateFunction } from "react-router-dom";
 
 export const signInWithGoogle = (
   setOpened: (flag: boolean) => void,
-  navigate: NavigateFunction
+  navigate: NavigateFunction,
+  setIsLoading: (loading: boolean) => void
 ) => {
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
+
+  setIsLoading(true); // start loading
+
   signInWithPopup(auth, provider)
     .then(async (result) => {
       setOpened(false);
+      setIsLoading(false); // end loading
+
       const user = result.user;
       const token = await user.getIdToken();
 
@@ -53,6 +59,8 @@ export const signInWithGoogle = (
     })
     .catch((error) => {
       setOpened(false);
+      setIsLoading(false); // end loading in case of an error
+
       if (error.code === "auth/account-exists-with-different-credential") {
         alert(
           `${error.customData.email}は他のSNSと連携した既存ユーザーが登録済みです。既存ユーザーでログイン後、こちらのSNSとの連携が可能です`
