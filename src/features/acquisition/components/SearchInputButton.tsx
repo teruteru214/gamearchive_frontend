f0c415050db1acdc1a484052f603229c4345e70f
teruteru <tutituti3080@gmail.com>
2023-07-30 17:02:57 +0900

    refactor: 検索フォームに空文字の場合、バリテーションエラーを表示する

--- src/features/acquisition/components/SearchInputButton.tsx
+++ src/features/acquisition/components/SearchInputButton.tsx
@@ -2,18 +2,31 @@ import { ActionIcon, TextInput } from "@mantine/core";
 import { IconPointerSearch, IconSearch } from "@tabler/icons-react";
 import { gameResultsAtom, searchQueryAtom } from "atoms/games/gameAcquisition";
 import { useAtom } from "jotai";
+import { useState } from "react";
+import { z } from "zod";
 
 import { searchGames } from "../api/searchGames";
 
 const SearchInputButton = () => {
   const [searchQuery, setSearchQuery] = useAtom(searchQueryAtom);
   const [, setGameResults] = useAtom(gameResultsAtom);
+  const [error, setError] = useState<string | null>(null);
+
+  const searchQuerySchema = z
+    .string()
+    .nonempty({ message: "*ゲーム名を入力してください" });
 
   const handleSearch = async () => {
     try {
+      searchQuerySchema.parse(searchQuery);
+      setError(null);
       await searchGames(searchQuery, setGameResults);
     } catch (error) {
-      console.error(error);
+      if (error instanceof z.ZodError) {
+        setError(error.errors[0].message);
+      } else {
+        console.error(error);
+      }
     }
   };
 
@@ -36,8 +49,8 @@ const SearchInputButton = () => {
             <IconPointerSearch size="1.1rem" stroke={1.5} />
           </ActionIcon>
         }
-        placeholder="ゲームの名前を入力してください"
         rightSectionWidth={42}
+        error={error}
       />
       <p className="text-xs text-gray-400">
         *目的のゲームが出てこない時、英語でも検索してみてください
