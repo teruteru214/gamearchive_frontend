4ec7c5a4150bcfe85fd1bf6c166e98c36e04d21c
teruteru <tutituti3080@gmail.com>
2023-08-17 15:31:38 +0900

    Add: ゲームデータを受け取る型を追加

--- src/features/management/types/index.ts
+++ src/features/management/types/index.ts
@@ -26,3 +26,37 @@ export type GameCard = {
     status: GameStatus;
   };
 };
+
+export type GameType = {
+  attributes: {
+    cover: string;
+    game_genres: {
+      id: number;
+      name: string;
+    }[];
+    game_platforms: {
+      id: number;
+      name: string;
+    }[];
+    game_status: {
+      id: number;
+      status: GameStatus;
+    };
+    id: number;
+    rating: number;
+    title: string;
+    url: string;
+  };
+};
+
+export type ResponseGameType = {
+  data: GameType[] | [];
+};
+
+export type GameListsType = {
+  game_status: {
+    name: string;
+    path: string;
+  }[];
+  gameItems: GameCard[];
+};
