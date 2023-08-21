834034146f9196416889a3d0ccc153a3d4f0b315
teruteru <tutituti3080@gmail.com>
2023-08-21 15:11:40 +0900

    Feat: 選択したgenreとplatformに関連するゲームカードのみ表示する。

--- src/features/management/components/RefineModal.tsx
+++ src/features/management/components/RefineModal.tsx
@@ -1,6 +1,8 @@
-import { Button, Divider, Modal, Text } from "@mantine/core";
+import { Button, Divider, Modal, Title } from "@mantine/core";
+import { useAtom } from "jotai";
 import { FC } from "react";
 
+import { selectedGenresAtom, selectedPlatformsAtom } from "../atoms";
 import { GameCard } from "../types";
 import { extractUniqueGenresAndPlatforms } from "../utils";
 
@@ -12,10 +14,28 @@ type RefineModalProps = {
 
 const RefineModal: FC<RefineModalProps> = ({ opened, onClose, gameItems }) => {
   const { genres, platforms } = extractUniqueGenresAndPlatforms(gameItems);
+  const [selectedGenres, setSelectedGenres] = useAtom(selectedGenresAtom);
+  const [selectedPlatforms, setSelectedPlatforms] = useAtom(
+    selectedPlatformsAtom
+  );
+
+  const handleGenreClick = (genre: string) => {
+    setSelectedGenres((prev) =>
+      prev.includes(genre) ? prev.filter((g) => g !== genre) : [...prev, genre]
+    );
+  };
+
+  const handlePlatformClick = (platform: string) => {
+    setSelectedPlatforms((prev) =>
+      prev.includes(platform)
+        ? prev.filter((p) => p !== platform)
+        : [...prev, platform]
+    );
+  };
 
   return (
     <Modal opened={opened} onClose={onClose} size="sm">
-      <Text>ジャンルで絞る</Text>
+      <Title order={5}>ゲームジャンルで絞る</Title>
       <Divider my="sm" />
       {genres.map((genre, index) => (
         <Button
@@ -24,13 +44,15 @@ const RefineModal: FC<RefineModalProps> = ({ opened, onClose, gameItems }) => {
           size="xs"
           compact
           className="mr-1"
-          variant="outline"
+          variant={selectedGenres.includes(genre) ? "filled" : "outline"}
+          onClick={() => handleGenreClick(genre)}
         >
           {genre}
         </Button>
       ))}
-      <Divider my="sm" />
-      <Text>プラットフォームで絞る</Text>
+      <Title order={5} className="mt-5">
+        ゲーム機体で絞る
+      </Title>
       <Divider my="sm" />
       {platforms.map((platform, index) => (
         <Button
@@ -39,7 +61,8 @@ const RefineModal: FC<RefineModalProps> = ({ opened, onClose, gameItems }) => {
           size="xs"
           compact
           className="mr-1"
-          variant="outline"
+          variant={selectedPlatforms.includes(platform) ? "filled" : "outline"}
+          onClick={() => handlePlatformClick(platform)}
         >
           {platform}
         </Button>
