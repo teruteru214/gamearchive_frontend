79e05cabd05d775b5d68dca1659be3b112c0c673
teruteru <tutituti3080@gmail.com>
2023-08-21 15:10:35 +0900

    Feat: 選択したgenreとplatformを格納するatomを作成

--- src/features/management/atoms/index.ts
+++ src/features/management/atoms/index.ts
@@ -3,3 +3,6 @@ import { atom } from "jotai";
 export const myGamesToShowAtom = atom<number>(14);
 
 export const sortOrderAtom = atom<"ascending" | "descending" | null>(null);
+
+export const selectedGenresAtom = atom<string[]>([]);
+export const selectedPlatformsAtom = atom<string[]>([]);
