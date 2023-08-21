65c1987719480d4247471f85bd1008457535c654
teruteru <tutituti3080@gmail.com>
2023-08-21 13:10:17 +0900

    Style: ゲームカードの操作ボタンのタイトルを付け足した。

--- src/features/management/container/GameManagementControls.tsx
+++ src/features/management/container/GameManagementControls.tsx
@@ -1,4 +1,4 @@
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useAtom } from "jotai";
 import { useNavigate } from "react-router-dom";
@@ -12,47 +12,52 @@ const GameManagementControls = () => {
   const [, setSortOrder] = useAtom(sortOrderAtom);
 
   return (
-    <div className="flex justify-center">
-      <Group position="center">
-        <Button
-          onClick={() => navigate("/acquisition")}
-          size="xs"
-          className="w-52"
-          variant="light"
-          radius="lg"
-        >
-          ゲームを追加する
-        </Button>
-        <Button
-          size="xs"
-          className="w-52"
-          onClick={open}
-          variant="light"
-          radius="lg"
-        >
-          ジャンルとゲーム機体で絞り込む
-        </Button>
-        <Button
-          size="xs"
-          className="w-52"
-          variant="light"
-          radius="lg"
-          onClick={() => setSortOrder("descending")}
-        >
-          評価が高い順に並べる
-        </Button>
-        <Button
-          size="xs"
-          className="w-52"
-          variant="light"
-          radius="lg"
-          onClick={() => setSortOrder("ascending")}
-        >
-          評価が低い順に並べる
-        </Button>
-        <RefineModal opened={opened} onClose={close} />
-      </Group>
-    </div>
+    <>
+      <Title order={4} className="mb-5" align="center">
+        絞り込み or 並べ替え
+      </Title>
+      <div className="flex justify-center">
+        <Group position="center">
+          <Button
+            onClick={() => navigate("/acquisition")}
+            size="xs"
+            className="w-52"
+            variant="light"
+            radius="lg"
+          >
+            ゲームを追加する
+          </Button>
+          <Button
+            size="xs"
+            className="w-52"
+            onClick={open}
+            variant="light"
+            radius="lg"
+          >
+            ジャンルとゲーム機体で絞り込む
+          </Button>
+          <Button
+            size="xs"
+            className="w-52"
+            variant="light"
+            radius="lg"
+            onClick={() => setSortOrder("descending")}
+          >
+            評価が高い順に並べる
+          </Button>
+          <Button
+            size="xs"
+            className="w-52"
+            variant="light"
+            radius="lg"
+            onClick={() => setSortOrder("ascending")}
+          >
+            評価が低い順に並べる
+          </Button>
+          <RefineModal opened={opened} onClose={close} />
+        </Group>
+      </div>
+    </>
   );
 };
 
