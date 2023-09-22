549d431158c5fb99433eeeadcda09fe88fa686f7
teruteru <tutituti3080@gmail.com>
2023-09-22 15:21:37 +0900

    Chore: ユーザーがlineログインしないようにヘッダーメニューからラインログインを削除

--- src/components/HeaderAction.tsx
+++ src/components/HeaderAction.tsx
@@ -11,7 +11,6 @@ import {
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import {
-  IconBell,
   IconDeviceGamepad2,
   IconDisc,
   IconLogout,
@@ -190,13 +189,6 @@ const HeaderAction = ({ isLogin }: HeaderActionProps) => {
                     onClick={() => navigate("/management/unplaying")}
                   >
                     ゲームマネジメント
-                  </Menu.Item>
-
-                  <Menu.Item
-                    icon={<IconBell size="0.9rem" stroke={1.5} />}
-                    onClick={() => navigate("/line")}
-                  >
-                    LINE通知設定(作成中)
                   </Menu.Item>
 
                   <Menu.Item
