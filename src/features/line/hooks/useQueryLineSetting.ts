193eace62b8ec07c3dbdeddf0dd3077b5ff69618
teruteru <tutituti3080@gmail.com>
2023-09-24 17:56:10 +0900

    Feat: データを取得し、管理するQueryを作成

---
+++ src/features/line/hooks/useQueryLineSetting.ts
@@ -0,0 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosResponse } from "axios";
+import { endpoint } from "config";
+import { getAuth } from "firebase/auth";
+
+import { ResponseLineSettingType } from "../types";
+
+export const useQueryLineSetting = () => {
+  const getLineSetting = async () => {
+    const auth = getAuth();
+    const idToken = await auth.currentUser?.getIdToken(true);
+    const uid = await auth.currentUser?.uid;
+    const config = {
+      headers: {
+        authorization: `Bearer ${idToken}`,
+      },
+    };
+    const res: AxiosResponse<ResponseLineSettingType> = await axios.get(
+      `${endpoint}/line_setting?uid=${uid}`,
+      config
+    );
+
+    const lineSettingData = res.data.data.attributes;
+
+    const LineSetting = {
+      id: lineSettingData.id,
+      line_notification: lineSettingData.line_notification,
+      favorite_notification_interval:
+        lineSettingData.favorite_notification_interval,
+      stacked_notification_interval:
+        lineSettingData.stacked_notification_interval,
+    };
+    return LineSetting;
+  };
+
+  const { status, data } = useQuery({
+    queryKey: ["lineSetting"],
+    queryFn: getLineSetting,
+    staleTime: Infinity,
+    onError: () => alert(`Line設定情報取得に失敗しました。`),
+  });
+
+  return {
+    data,
+    status,
+  };
+};
