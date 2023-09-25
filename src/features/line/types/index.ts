9720f195bb8c6d0cd50ef34e72783e5a93c2a4f8
teruteru <tutituti3080@gmail.com>
2023-09-25 13:03:20 +0900

    Add: 型定義を追加

--- src/features/line/types/index.ts
+++ src/features/line/types/index.ts
@@ -24,3 +24,30 @@ export type LineSettingType = {
 export type ResponseLineSettingType = {
   data: LineSettingType;
 };
+
+export interface UpdateLineSettingParams {
+  id: number;
+  line_notification: boolean;
+  stacked_notification_interval: number;
+  favorite_notification_interval: number;
+  config: { headers: { authorization: string } };
+}
+
+export interface UserLineQueryResult {
+  data:
+    | {
+        id: number;
+        line_notification: boolean;
+        favorite_notification_interval: number;
+        stacked_notification_interval: number;
+      }
+    | undefined;
+  status: "error" | "success" | "loading";
+}
+
+export interface NotificationSettingsProps {
+  userId?: number;
+  initialIsSwitchOn?: boolean;
+  initialStackedValue?: number;
+  initialFavoriteValue?: number;
+}
