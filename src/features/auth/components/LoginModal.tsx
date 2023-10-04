34d8c728bf9fd195fbd7f250de82faeb7e029ac5
teruteru <tutituti3080@gmail.com>
2023-10-05 00:34:25 +0900

    Style: Loginモーダルのデザインを一新

--- src/features/auth/components/LoginModal.tsx
+++ src/features/auth/components/LoginModal.tsx
@@ -2,6 +2,7 @@ import { Group, Image, Modal, Stack, Text } from "@mantine/core";
 import GoogleButton from "components/Button/GoogleButton";
 import { signInWithGoogle } from "lib/auth/auth";
 import { FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import logo from "../../../assets/logo.png";
 
@@ -12,13 +13,19 @@ type LoginModalProps = {
 
 const LoginModal: FC<LoginModalProps> = ({ opened, close }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <Modal opened={opened} onClose={close} centered size="md">
       <Stack className="flex items-center justify-center space-y-4">
         <Image src={logo} width={300} fit="contain" />
         <Text size="sm" color="dimmed">
-          GameArchiveは、あなたの全てのゲーム進行状況を一元化し、効率的なプレイを実現するためのアプリケーションです。ここでは、複数のプラットフォームのゲームを一覧表示し、プレイ状況を一目で確認することができます
+          ログインすると以下の機能が使用可能になります
+          <ul>
+            <li>ゲームが保存可能になります</li>
+            <li>ゲームマネジメントが使えるようになります</li>
+            <li>積みゲー通知設定ができるようになります</li>
+          </ul>
         </Text>
         <Group grow mb="md" mt="md">
           <GoogleButton
@@ -28,7 +35,34 @@ const LoginModal: FC<LoginModalProps> = ({ opened, close }) => {
           />
         </Group>
         <Text size="sm" color="dimmed" className="px-5 text-center">
-          利用規約、プライバシーポリシーに同意したうえでログインしてください。
+          <span
+            onClick={() => navigate("/terms")}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === " ") {
+                navigate("/terms");
+              }
+            }}
+            role="button"
+            tabIndex={0}
+            className="cursor-pointer text-blue-500 hover:underline"
+          >
+            利用規約
+          </span>
+          、
+          <span
+            onClick={() => navigate("/privacy-policy")}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === " ") {
+                navigate("/privacy-policy");
+              }
+            }}
+            role="button"
+            tabIndex={0}
+            className="cursor-pointer text-blue-500 hover:underline"
+          >
+            プライバシーポリシー
+          </span>
+          に同意したうえでログインしてください。
         </Text>
       </Stack>
     </Modal>
